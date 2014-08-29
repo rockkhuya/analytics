fe61c724b78b4fb771cdcc8167bfc02060696150
Luu Tuan Anh <anh@okwave.co.jp>
2014-08-29 14:47:17 +0900

    fixed

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,12 +6,14 @@ class WelcomeController < ApplicationController
 
    def analytics
     return :back unless current_user
-    return if @result = Result.find_by(identifier: current_user.identifier)
 
     @me = FbGraph::User.me(current_user.access_token).fetch if current_user
     @page = FbGraph::Page.new(112562602433)
     return :back unless @me.like?(@page)
 
+    return if @result = Result.find_by(identifier: current_user.identifier)
+
     @result = Result.new(identifier: current_user.identifier).prediction
+    p @me.inspect
   end
 end
